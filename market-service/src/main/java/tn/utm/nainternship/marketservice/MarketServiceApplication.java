07ef9ac8fc6f36fe3ae777b3a861fc8da20c9651
joe-9104 <fathallahyoussef09@gmail.com>
2026-06-15 11:00:12 +0100

    Market service : corrected package names and created application-dev.yml

--- market-service/src/main/java/tn/utm/nainternship/marketservice/MarketServiceApplication.java
+++ market-service/src/main/java/tn/utm/nainternship/marketservice/MarketServiceApplication.java
@@ -1,4 +1,4 @@
-package tn.utm;
+package tn.utm.nainternship.marketservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

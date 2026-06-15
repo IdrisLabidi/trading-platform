c3aa2648a78b18081e65b9bf5680cc10728a8d6b
joe-9104 <fathallahyoussef09@gmail.com>
2026-06-15 14:51:22 +0100

    création de Portfolio Service

---
+++ portfolio-service/src/test/java/tn/utm/nainternship/portfolioservice/PortfolioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.utm.nainternship.portfolioservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

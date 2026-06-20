b229198fdf33f3095abc67f764a7f9ab9c8582b5
IdrisLaabidi <idrislaabidi@gmail.com>
2026-06-20 18:39:49 +0100

    add gRPC service (config incomplete) and test configuration; include AssetService proto definition and H2 database setup

--- asset-service/src/test/java/tn/utm/nainternship/assetservice/AssetServiceApplicationTests.java
+++ asset-service/src/test/java/tn/utm/nainternship/assetservice/AssetServiceApplicationTests.java
@@ -2,8 +2,10 @@ package tn.utm.nainternship.assetservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AssetServiceApplicationTests {
 
     @Test
